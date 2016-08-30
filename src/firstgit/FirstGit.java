66f672c284a30285444bd85780758e421d29150a
amellem <amellem@L11105042790.wctc.msft>
2016-08-30 16:28:03 -0500

    Added Animal Class Modified Main to include a second message

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -12,7 +12,7 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello Java");
-        System.out.println("Hello Class");
+        System.out.println("Hello Advanced Java");
     }
     
 }
