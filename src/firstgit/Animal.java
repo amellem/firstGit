ec184c963be7a22283731274f37c9d530b6032c9
amellem <amellem@L11105042790.wctc.msft>
2016-08-30 16:43:31 -0500

    Added Person class and code to Animal class

--- src/firstgit/Animal.java
+++ src/firstgit/Animal.java
@@ -6,5 +6,23 @@ package firstgit;
  * @author amellem
  */
 public class Animal {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+      if(validateAge(age)){
+          this.age = age;
+      }
+    }
     
+    private boolean validateAge(int age){
+          if(age < 0 || age > 999){
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
---
+++ src/firstgit/Person.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstgit;
+
+/**
+ *
+ * @author amellem
+ */
+public class Person {
+    public void adaptAnimal(){
+        Animal animal = new Animal();
+        animal.setAge(5);
+    }
+}
