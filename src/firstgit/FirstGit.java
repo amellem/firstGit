71ee6a55c7eaed27b624b9a7da6603af3956c891
amellem <amellem@L11105042790.wctc.msft>
2016-08-30 16:18:29 -0500

    Revert "Revert "Added Animal Class Modified Main to include a second message""
    This reverts commit 56e1989ae6b0cc401d5a4a697af939be522db8d3.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -11,7 +11,8 @@ public class FirstGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Java");
+        System.out.println("Hello Class");
     }
     
 }
