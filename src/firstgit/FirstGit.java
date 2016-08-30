ae3a6301f71b3659337397cd58d6632ac0fb57e1
amellem <amellem@L11105042790.wctc.msft>
2016-08-30 16:02:15 -0500

    First commit

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,17 @@
+
+package firstgit;
+
+/**
+ *
+ * @author amellem
+ */
+public class FirstGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
